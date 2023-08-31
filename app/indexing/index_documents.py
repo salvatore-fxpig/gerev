a7184074de00a1c744f4f8fc42ae2c82819e13d0
Salvatore Cunsolo <sal.cuns@gmail.com>
2023-08-31 12:58:33 +0200

    remove random dead code causing errors

--- app/indexing/index_documents.py
+++ app/indexing/index_documents.py
@@ -8,10 +8,8 @@ from db_engine import Session
 from indexing.bm25_index import Bm25Index
 from indexing.faiss_index import FaissIndex
 from models import bi_encoder
-from parsers.pdf import split_PDF_into_paragraphs
 from paths import IS_IN_DOCKER
 from schemas import Document, Paragraph
-from langchain.schema import Document as PDFDocument
 
 
 logger = logging.getLogger(__name__)
